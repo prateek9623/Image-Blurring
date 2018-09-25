af0b1131e7b1f142e15a73c255edf8e0dba053c5
Prateek Chokse <prateek.chokse@live.com>
2018-09-25 13:35:00 +0530

    0.1

--- Image-Blurring/kernel.cu
+++ Image-Blurring/kernel.cu
@@ -1,121 +1,213 @@
-
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <cuda.h>
+#include <device_functions.h>
+#include <cuda_runtime_api.h>
+#include "time.h";
+#include <curand.h>
+#include <curand_kernel.h>
+#include <iostream>
 
-#include <stdio.h>
+const int RADIUS = 1;
+const int MATRIX_SIZE = 18;
+const int MAX = 10;
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+using namespace std;
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+void fillRandom(int *matrix, int maxX, int maxY, int range, unsigned long seed)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	srand(seed);
+	for (int i = 0; i < maxX; i++)
+		for (int j = 0; j < maxY; j++)
+			*((matrix + i * maxY) + j) = rand() % MAX;
+
+	//*((matrix + i * maxY) + j) = 10;
 }
+
+__device__ void memSetSharedMem(int x, int y, int *sharedData, int *globalData, int maxX, int maxY) {
+	if (threadIdx.x == 0 && threadIdx.y == 0) {
+		for (int posY = 0; posY <= RADIUS; posY++)
+			for (int posX = 0; posX <= RADIUS; posX++) {
+				sharedData[posX + posY * (blockDim.x + 2 * RADIUS)] = globalData[(x - RADIUS + posX) + maxX * (y - RADIUS + posY)];
+			}
+	}
+	else if (threadIdx.y == 0 && threadIdx.x == blockDim.x - 1) {
+		for (int posY = 0; posY <= RADIUS; posY++)
+			for (int posX = 0; posX <= RADIUS; posX++) {
+				sharedData[posX + threadIdx.x + RADIUS + posY * (blockDim.x + 2 * RADIUS)] = globalData[(x + posX) + maxX * (y - RADIUS + posY)];
+			}
+	}
+	else if (threadIdx.y == blockDim.y - 1 && threadIdx.x == 0) {
+		for (int posY = 0; posY <= RADIUS; posY++)
+			for (int posX = 0; posX <= RADIUS; posX++) {
+				sharedData[posX + (posY + RADIUS + threadIdx.y) * (blockDim.x + 2 * RADIUS)] = globalData[(x - RADIUS + posX) + maxX * (y + posY)];
+			}
+	}
+	else if (threadIdx.y == blockDim.y - 1 && threadIdx.x == blockDim.x - 1) {
+		for (int posY = 0; posY <= RADIUS; posY++)
+			for (int posX = 0; posX <= RADIUS; posX++) {
+				sharedData[posX + RADIUS + threadIdx.x + (posY + RADIUS + threadIdx.y) * (blockDim.x + 2 * RADIUS)] = globalData[(x + posX) + maxX * (y + posY)];
+			}
+	}
+	else if (threadIdx.x == 0) {
+		int posY = threadIdx.y + RADIUS;
+		for (int posX = 0; posX <= RADIUS; posX++) {
+			sharedData[posX + posY * (blockDim.x + 2 * RADIUS)] = globalData[(x - RADIUS + posX) + maxX * y];
+		}
+	}
+	else if (threadIdx.y == 0) {
+		int posX = threadIdx.x + RADIUS;
+		for (int posY = 0; posY <= RADIUS; posY++) {
+			sharedData[posX + posY * (blockDim.x + 2 * RADIUS)] = globalData[x + maxX * (y - RADIUS + posY)];
+		}
+	}
+	else if (threadIdx.x == blockDim.x - 1) {
+		int posY = threadIdx.y + RADIUS;
+		for (int posX = 0; posX <= RADIUS; posX++) {
+			sharedData[posX + RADIUS + threadIdx.x + posY * (blockDim.x + 2 * RADIUS)] = globalData[(x + posX) + maxX * y];
+		}
+	}
+	else if (threadIdx.y == blockDim.y - 1) {
+		int posX = threadIdx.x + RADIUS;
+		for (int posY = 0; posY <= RADIUS; posY++) {
+			sharedData[posX + (posY + RADIUS + threadIdx.y) * (blockDim.x + 2 * RADIUS)] = globalData[x + maxX * (y + posY)];
+		}
+	}
+	else {
+		sharedData[threadIdx.x + RADIUS + (threadIdx.y + RADIUS) * (blockDim.x + 2 * RADIUS)] = globalData[x + maxX * y];
+	}
+}
+
+__global__ void findAverage(int *matrix, int *avgMatrix, int maxX, int maxY, int count) {
+	int x = threadIdx.x + blockIdx.x*blockDim.x + RADIUS;
+	int y = threadIdx.y + blockIdx.y*blockDim.y + RADIUS;
+
+	extern __shared__ int sharedData[];
+
+
+
+	if (x < maxX - RADIUS && y < maxY - RADIUS) {
+		memSetSharedMem(x, y, sharedData, matrix, maxX, maxY);
+		__syncthreads();
+		int sum = 0;
+		int sharedMaxX = blockDim.x + 2 * RADIUS;
+		//if (threadIdx.x == 0 && blockIdx.x == 3 && threadIdx.y == 2 && blockIdx.y == 3)
+		{
+			for (int r = 0; r < 2 * RADIUS + 1; r++) {
+				for (int c = 0; c < 2 * RADIUS + 1; c++) {
+					//printf("%d ", sharedData[(threadIdx.x + c) + (sharedMaxX)*(threadIdx.y + r)]);
+					sum += sharedData[(threadIdx.x + c) + (sharedMaxX)*(threadIdx.y + r)];
+				}
+				//printf("\n");
+			}
+			avgMatrix[x + maxX * y] = sum / count;
+		}
+	}
+
+}
+//__global__ void findAverage(int *matrix, int *avgMatrix, int maxX, int maxY, int radius, int count) {
+//	int x = threadIdx.x + blockIdx.x*blockDim.x;
+//	int y = threadIdx.y + blockIdx.y*blockDim.y;
+//	int index = x + maxX*y;
+//	
+//	if (x < maxX && y < maxY) {
+//		int sum = 0;
+//		int cou = 0;
+//		for (int offsetY = y - radius; offsetY <= y + radius && offsetY < maxY; offsetY++) {
+//			for (int offsetX = x - radius; offsetX <= x + radius && offsetX < maxX; offsetX++) {
+//				if (offsetX >= 0 && offsetY >= 0)
+//				{
+//					int indexOffset = offsetY * maxX + offsetX;
+//					sum += matrix[indexOffset];
+//				}
+//			}
+//		}
+//		avgMatrix[index] = sum / count;
+//		//__syncthreads();
+//		//printf("%d ", avgMatrix[index]);
+//	}
+//}
+
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+	int *matrix = new int[MATRIX_SIZE*MATRIX_SIZE];
+	int *avgMatrix = new int[MATRIX_SIZE*MATRIX_SIZE];
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	int *dMatrix;
+	int *dAvgMatrix;
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	cudaFree(0);
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+	fillRandom((int*)matrix, MATRIX_SIZE, MATRIX_SIZE, 10, time(NULL));
+	int totalElements = MATRIX_SIZE * MATRIX_SIZE;
 
-    return 0;
-}
+	if (cudaMalloc(&dMatrix, sizeof(int)*totalElements) != cudaSuccess) {
+		cerr << "Couldn't allocate memory for matrix";
+		cudaFree(dMatrix);
+	};
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
+	if (cudaMalloc(&dAvgMatrix, sizeof(int)*totalElements) != cudaSuccess) {
+		cerr << "Couldn't allocate memory for Average Matrix";
+		cudaFree(dAvgMatrix);
+	};
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+	if (cudaMemcpy(dMatrix, matrix, sizeof(int)*totalElements, cudaMemcpyHostToDevice) != cudaSuccess) {
+		cerr << "Couldn,t initialiZe device Original Matrix";
+		cudaFree(dMatrix);
+		cudaFree(dAvgMatrix);
+	}
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	if (cudaMemcpy(dAvgMatrix, matrix, sizeof(int)*totalElements, cudaMemcpyHostToDevice) != cudaSuccess) {
+		cerr << "Couldn,t initialiZe device Average Matrix";
+		cudaFree(dMatrix);
+		cudaFree(dAvgMatrix);
+	}
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	const dim3 blockSize(16, 16);
+	const dim3 gridSize((MATRIX_SIZE - 2 * RADIUS + blockSize.x - 1) / blockSize.x, (MATRIX_SIZE - 2 * RADIUS + blockSize.y - 1) / blockSize.y);
+	int count = (RADIUS * 2 + 1)*(RADIUS * 2 + 1);
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	int sharedMemSpace = (blockSize.x + 2 * RADIUS)*(blockSize.y + 2 * RADIUS);
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	findAverage << <gridSize, blockSize, sharedMemSpace * sizeof(int) >> > (dMatrix, dAvgMatrix, MATRIX_SIZE, MATRIX_SIZE, count);
+	//findAverage <<<gridSize, blockSize >>> (dMatrix, dAvgMatrix, MATRIX_SIZE, MATRIX_SIZE, RADIUS, count);
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	cudaDeviceSynchronize();
 
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+	if (cudaGetLastError() != cudaSuccess) {
+		cerr << "kernel launch failed: " << cudaGetErrorString(cudaGetLastError());
+		cudaFree(dMatrix);
+		cudaFree(dAvgMatrix);
+		exit(1);
+	}
 
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
+	if (cudaMemcpy(avgMatrix, dAvgMatrix, sizeof(int)*totalElements, cudaMemcpyDeviceToHost) != cudaSuccess) {
+		cerr << "Couldn't copy original matrix memory from device to host";
+		cudaFree(dMatrix);
+		cudaFree(dAvgMatrix);
+		exit(1);
+	}
+	cudaFree(dAvgMatrix);
+	cudaFree(dMatrix);
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	cout << endl << endl;
+	for (int i = 0; i < MATRIX_SIZE; i++) {
+		for (int j = 0; j < MATRIX_SIZE; j++) {
+			cout << *((matrix + i * MATRIX_SIZE) + j) << " ";
+		}
+		cout << endl;
+	}
 
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+	cout << endl << endl;
+	for (int i = 0; i < MATRIX_SIZE; i++) {
+		for (int j = 0; j < MATRIX_SIZE; j++) {
+			cout << *((avgMatrix + i * MATRIX_SIZE) + j) << " ";
+		}
+		cout << endl;
+	}
+
+	delete[] avgMatrix;
+	delete[] matrix;
+	return 0;
 }
